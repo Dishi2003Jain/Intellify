56cce14c18adc795af49efe6c18e1681b9209358
Dishi <dishinayak223@gmail.com>
2023-12-25 04:22:25 +0530

    intellifygitcomponent

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/Header';
-import CourseList from './components/CourseList';
+import Header from '../components/Header';
+import CourseList from '../components/CourseList';
 
 function home() {
   return (
